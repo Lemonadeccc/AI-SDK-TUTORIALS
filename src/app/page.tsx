63f666efdfabfaf9fd4237de3d71270d56a2a585
root <root@v2202601302628423879.bestsrv.de>
2026-01-10 07:33:21 +0100

    feat: add tools call

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,6 +34,9 @@ export default function Home() {
       <div>
         <Link href="/ui/generate-speech">Generate Speech</Link>
       </div >
+      <div>
+        <Link href="/ui/tools">Tools</Link>
+      </div>
     </>
   );
 }
